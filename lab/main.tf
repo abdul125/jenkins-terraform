2b4bafdfb9019db149510609e29ac23d63637f20
Abdulrahman <17806978+abdul125@users.noreply.github.com>
2020-08-13 10:11:41 +0300

    Update main.tf

--- lab/main.tf
+++ lab/main.tf
@@ -198,7 +198,7 @@ resource "aws_instance" "api" {
   count                       = 1
   ami                         = data.aws_ami.latest_webserver.id
   instance_type               = var.instance_type
-  subnet_id                   = aws_subnet.webserver[count.index].id
+  subnet_id                   = aws_subnet.webserver[0].id
   vpc_security_group_ids      = [aws_security_group.webserver.id]
   key_name                    = aws_key_pair.lab_keypair.id
   associate_public_ip_address = true
